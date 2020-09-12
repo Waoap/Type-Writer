5b64f613b660fe6217fc009a9d571bc929e41d2b
Waoap <51262009+Waoap@users.noreply.github.com>
2020-09-12 21:57:05 +0800

    Source Code
    Add source code

---
+++ TypeWriter.kt
@@ -0,0 +1,59 @@
+package com.shabby.seamless.widget
+
+import android.content.Context
+import android.os.Handler
+import android.util.AttributeSet
+import androidx.appcompat.widget.AppCompatTextView
+import com.shabby.seamless.R
+
+/**
+ * Type Writer
+ *
+ * Based on Antarix's project
+ * on 'https://gist.github.com/Antarix/6388606'
+ *
+ * @author Shabby
+ */
+class TypeWriter(context: Context, attrs: AttributeSet?, defStyleAttr: Int) :
+    AppCompatTextView(context, attrs, defStyleAttr) {
+    private var mText: CharSequence = text ?: ""
+    private var mIndex = 0
+    private var mDelay: Long = 150 // Default 150ms delay
+    private var mTypeArray = context.obtainStyledAttributes(attrs, R.styleable.TypeWriter)
+
+    constructor(context: Context) : this(context, null)
+
+    constructor(context: Context, attrs: AttributeSet?)
+            : this(context, attrs, android.R.attr.textViewStyle) {
+        text = ""
+        mDelay = mTypeArray.getInt(R.styleable.TypeWriter_delay, 150).toLong()
+        if (mTypeArray.getBoolean(R.styleable.TypeWriter_autoStart, false)) startAnimation()
+    }
+
+    private val mHandler = Handler()
+    private val characterAdder: Runnable = object : Runnable {
+        override fun run() {
+            text = mText.subSequence(0, mIndex++)
+            if (mIndex <= mText.length) {
+                mHandler.postDelayed(this, mDelay)
+            }
+        }
+    }
+
+    fun startAnimation() {
+        mIndex = 0
+        mHandler.removeCallbacks(characterAdder)
+        mHandler.postDelayed(characterAdder, mDelay)
+    }
+
+    fun startAnimation(text: CharSequence) {
+        mIndex = 0
+        mText = text
+        mHandler.removeCallbacks(characterAdder)
+        mHandler.postDelayed(characterAdder, mDelay)
+    }
+
+    fun needTime(): Long {
+        return (mText.length + 1) * mDelay
+    }
+}
